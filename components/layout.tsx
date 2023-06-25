50aa250b0919a3e5c4bb9988e1409aed8dcc127b
floritfran <fflorit@fi.uba.ar>
2023-06-25 16:25:38 -0300

    Estilo de pantalla de proyectos

--- components/layout.tsx
+++ components/layout.tsx
@@ -33,11 +33,9 @@ export default function Layout({ children }: { children: any }) {
 
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <header className="bg-black sticky top-0 h-14 flex justify-center items-center font-semibold uppercase text-white">
-        PSA
-      </header>
       <div className="flex flex-col md:flex-row flex-1">
-        <aside className="bg-grey-100 w-full md:w-60">
+        <aside className="bg-grey-100 w-full md:w-60" style={{backgroundColor: "#0F3A61"}}>
+          <div style={{display: 'flex', fontSize: 40, marginLeft: 30, marginTop: 30, marginBottom: 30, color: "#FFFFFF"}}>PSA</div>
           <nav>
             <ul>
               {menuItems.map((item) => (
--- pages/proyectos.tsx
+++ pages/proyectos.tsx
@@ -3,56 +3,58 @@ import ProjectGridRow from "@/components/proyectoGridRow";
 import { Project } from "./types";
 
 function HeaderItem({ title }: { title: string }) {
-    return <th className="px-6 py-3 text-sm text-left text-gray-500 border-b border-gray-200 bg-gray-50">{title}</th>
+    return <th className="px-6 py-3 text-sm text-left text-gray-500 border-b border-gray-200 bg-gray-50">{title}</th>;
+}
+
+function ProjectItem({name, client, id} : {name: string, client: string, id: number}) {
+    return (
+        <div style={{backgroundColor: "#0F3A61", color: "#FFFFFF", display: "flex", justifyContent: "space-between", marginBottom: 30, height: 100, alignItems: "center", padding: 20}}>
+            <div style={{fontSize: 20}}>
+                <div>{name}</div>
+                <div>{client}</div>
+            </div>
+            <div style={{display: "flex", fontSize: 18}}>
+                <button 
+                    style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#248CED", borderRadius: 10, height: 42, width: 95}}
+                    onClick={() => window.open(`http://localhost:3000/proyectos/${id}`, "_self")}
+                >Ver</button>
+                <button style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#7B7B7B", borderRadius: 10, height: 42, width: 95, marginLeft: 12}}>Editar</button>
+                <button style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#D73838", borderRadius: 10, height: 42, width: 95, marginLeft: 12}}>Eliminar</button>
+            </div>
+        </div>
+    );
 }
 
 export default function Proyectos() {
-    const [list, setList] : [Project[], Function] = useState([])
+    const [list, setList] : [Project[], Function] = useState([]);
 
     useEffect(() => {
-        // Traerse los proyectos del endpoint
+        // Se trae los proyectos del endpoint
+        /* fetch("https://projects-backend-service.onrender.com/spec/projects")
+            .then((res) => {
+                console.log("res", res)
+                return res.json()
+            })
+            .then((data) => {
+                setList(data);
+            }
+        ); */
         setList([
             {id: 1, description: "", state: "En progreso", startDate: 1687210330000, endDate: 1687211330000, estimatedHours: 200, projectLeader: "Juan Nardoni", name: "QuickFix", client: "LDE"},
             {id: 2, description: "", state: "Sin iniciar", startDate: 1687210330000, endDate: 1687211330000, estimatedHours: 150, projectLeader: "Nahuel Molina", name: "GreenTech Solutions", client: "LOREM SRL"},
-            {id: 3, description: "", state: "Etapa final", startDate: 1687210330000, endDate: 1687211330000, estimatedHours: 20, projectLeader: "Pedro Gómez", name: "ABC Software Solutions", client: "KNN"}])
+            {id: 3, description: "", state: "Etapa final", startDate: 1687210330000, endDate: 1687211330000, estimatedHours: 20, projectLeader: "Pedro Gómez", name: "ABC Software Solutions", client: "KNN"}
+        ]);
     }, [])
 
     return (
-        <>
-            {/* ACA EMPIEZA LA GRILLA */}
-
-            <div className="container max-w-7xl mx-auto mt-8">
-                <div className="mb-4">
-                    <h1 className="text-3xl font-bold decoration-gray-400">Proyectos</h1>
-                </div>
-                <div className="flex flex-col">
-                    <div className="overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-                        <div className="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
-                            <table className="min-w-full">
-                                <thead>
-                                <tr>
-                                    <HeaderItem title="Nombre"/>
-                                    <HeaderItem title="Lider de proyecto"/>
-                                    <HeaderItem title="Cliente"/>
-                                    <HeaderItem title="Estado"/>
-                                    <HeaderItem title="Fecha inicio"/>
-                                    <HeaderItem title="Fecha fin estimado"/>
-                                    <HeaderItem title="Horas estimadas"/>
-                                    <HeaderItem title="Descripción"/>
-                                    <HeaderItem title="Acción"/>
-                                </tr>
-                                </thead>
-
-                                <tbody>
-                                {list.map((project, index) => (
-                                    <ProjectGridRow project={project} key={index}/>
-                                ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+        <div style={{backgroundColor: "#DDDDDC", display: "flex", flexDirection: "column", height: "100%", padding: 90}}>
+            <div style={{display: "flex", backgroundColor: "#185FA1", color: "#FFFFFF", justifyContent: "space-between", paddingLeft: 30, paddingRight: 30, height: 100, alignItems: "center", marginBottom: 30}}>
+                <div style={{fontSize: 20}}>Crear Proyecto</div>
+                <button style={{display:"flex", justifyContent: "center", alignItems: "center", borderRadius: 35, backgroundColor: "#248CED", height: 70, width: 70, fontSize: 40}}>+</button>
             </div>
-        </>
+            {list.map((project, index) => (
+                <ProjectItem name={project.name} client={project.client} id={project.id} key={index}/>
+            ))}
+        </div>
     )
 }
