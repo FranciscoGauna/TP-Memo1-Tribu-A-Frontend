6ca4665c2afe5dabbcf1046b7a5142e0ff9a8341
fedacking <fedacking@gmail.com>
2023-06-28 04:23:46 -0300

    Update [id].tsx

--- pages/proyectos/[id].tsx
+++ pages/proyectos/[id].tsx
@@ -1,15 +1,52 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Project } from "../types";
+import { Task, Project } from "../types";
+
+function TaskItem({task, setModalUpdateOpen, setModalDeleteOpen, setTask} : 
+	{task: Task, setModalUpdateOpen: Function, setModalDeleteOpen: Function, setTask: Function }) {
+    return (
+        <div style={{backgroundColor: "#0F3A61", color: "#FFFFFF", display: "flex", justifyContent: "space-between", marginBottom: 30, height: 100, alignItems: "center", padding: 20}}>
+            <div style={{fontSize: 20}}>
+                <div>{task.name}</div>
+                <div>{}</div>
+            </div>
+            <div style={{display: "flex", fontSize: 18}}>
+                <button
+                    style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#7B7B7B", borderRadius: 10, height: 42, width: 95, marginLeft: 12}}
+                    onClick={() => {
+                        setTask(task);
+                        setModalUpdateOpen(true);
+                    }}
+                >Editar</button>
+                <button 
+                    style={{display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#D73838", borderRadius: 10, height: 42, width: 95, marginLeft: 12}}
+                    onClick={() => {
+                        setTask(task);
+                        setModalDeleteOpen(true);
+                    }}
+                >Eliminar</button>
+            </div>
+        </div>
+    );
+}
 
 export default function ProyectoDetalle() {
 	const router = useRouter();
 	const [id, setId] : [string, Function] = useState("");
+	const [task, setTask] : [Task, Function] = useState({
+		puid: "",
+        name: "",
+        description: "",
+        state: "",
+        start_date: "",
+        end_date: "",
+        estimated_hours: 0,
+	});
     const [project, setProject] : [Project, Function] = useState({
         id: 0,
         name: "",
         description: "",
-        state: "",
+        stage: "",
         start_date: "",
         end_date: "",
         estimated_hours: 0,
@@ -17,6 +54,9 @@ export default function ProyectoDetalle() {
         tasks: {},
         uid: ""
     });
+
+	const setModalUpdateOpen = () => {};
+	const setModalDeleteOpen = () => {};
 	
 	useEffect(() => {
 		const url = window.location.href;
@@ -34,12 +74,23 @@ export default function ProyectoDetalle() {
             });
 	}, []);
 
-	return (<>
-		<div>Proyecto: {project.name}</div>
-		<div style={{display: 'flex', justifyContent: 'space-between', margin: 20}}>
-			<div>Tareas</div>
-			<button style={{backgroundColor: '#FBF784', padding: 10, borderRadius: 5}}>Crear tarea</button>
+	return (
+	<div style={{backgroundColor: "#DDDDDC", display: "flex", flexDirection: "column", height: "100%", padding: 90}}>
+		<div>
+			<p style={{color: "#000000"}}>Proyecto: {project.name}</p>
 		</div>
+		<div style={
+			{display: "flex", backgroundColor: "#185FA1", color: "#FFFFFF", justifyContent: "space-between", 
+			paddingLeft: 30, paddingRight: 30, height: 100, alignItems: "center", marginBottom: 30}}>
+            <div style={{fontSize: 20}}>Crear Tarea</div>
+            <button 
+                style={{display:"flex", justifyContent: "center", alignItems: "center", borderRadius: 35, backgroundColor: "#248CED", height: 70, width: 70, fontSize: 40}}
+                onClick={() => {}}
+            >+</button>
+        </div>
 		{/* Aca va la tabla de tareas */}
-	</>);
+		{Object.values(project.tasks).map((task, index) => (
+                <TaskItem task={task} setModalUpdateOpen={setModalUpdateOpen} setModalDeleteOpen={setModalDeleteOpen} setTask={setTask} key={index}/>
+            ))}
+	</div>);
 }
