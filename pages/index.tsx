3a1589967976093e1f1b3e8b7e7df541ed83b34e
fedacking <fedacking@gmail.com>
2023-06-29 19:33:52 -0300

    frente

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div className="flex h-full flex-col justify-center items-center bg-white">
-      <h1 className="text-4xl mb-5 font-bold">Home</h1>
-      <span className="text-7xl">🏡</span>
+		<h1 className="text-4xl mb-5 font-bold">Praxis Systems Argentina</h1>
     </div>
   )
 }
